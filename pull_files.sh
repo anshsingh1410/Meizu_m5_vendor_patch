c0e1f8aeee439c7930621c6c1e77702fce61a907
Ansh Singh <anshsingh.14.lko@gmail.com>
2018-07-23 21:17:42 +0530

    Update pull_files.sh

--- pull_files.sh
+++ pull_files.sh
@@ -3,4 +3,4 @@
 LOCAL_PATH="/home/Meizu_m5_vendor_patch"
 LOCAL_PATCH="/default" #Depends on patch you need.
 #adb
-adp pull $LOCAL_PATH$LOCAL_PATCH /system
+adb pull $LOCAL_PATH$LOCAL_PATCH /system
